03eee61041001858804208a48f7805373b982d71
Ana Alonso <alonsoana60@gmail.com>
2023-12-03 12:34:12 +0100

    details dos alunos com nome das materias

--- dist/details.js
+++ dist/details.js
@@ -7,30 +7,71 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-const values = window.location.search;
-const searchParams = new URLSearchParams(values);
+let alunos;
+let turmas = [];
+let materias = {};
+// Puxar turmas
+function getTurma() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const resp = yield fetch("http://127.0.0.1:3500/lista-turmas");
+        const turma = yield resp.json();
+        // console.log(turma);
+        turmas = turma;
+    });
+}
+// Puxar materias
+function getMateria() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const resp = yield fetch("http://127.0.0.1:3500/lista-materia");
+        const materia = yield resp.json();
+        console.log(materia);
+        materias = materia;
+    });
+}
+// Puxar alunos
 function getStudent(id) {
     return __awaiter(this, void 0, void 0, function* () {
         const resp = yield fetch(`http://localhost:3500/lista-alunos/${id}`);
-        const data = yield resp.json();
-        console.log(data);
-        function dataStudent(id2) {
-            const cont = document.querySelector(".container");
-            if (id2 === id) {
-                cont.innerHTML =
-                    `<span>Nome: ${data.nome}</span><br />
-        <span>Turma: ${data.turma}</span><br />`;
-            }
-            else {
-                cont.innerHTML = `Página Invalida`;
-            }
-        }
-        dataStudent(data.id);
+        const aluno = yield resp.json();
+        // console.log(aluno);
+        alunos = aluno;
     });
 }
+const values = window.location.search;
+const searchParams = new URLSearchParams(values);
 if (searchParams.has("id")) {
     console.log("id", searchParams.get("id"));
     const id = parseInt(searchParams.get("id"));
-    getStudent(id);
+    Promise.all([getMateria(), getTurma(), getStudent(id)]).then(() => {
+        console.log(alunos, turmas, materias);
+        // popular nome aluno
+        document.querySelector("#nome").textContent = alunos.nome;
+        // popular turma do aluno
+        const turma = turmas.find((turma) => turma.id === alunos.turma);
+        document.querySelector("#turma").textContent = turma === null || turma === void 0 ? void 0 : turma.turma;
+        // popular materias
+        const notas = document.querySelector("tbody");
+        console.log(notas);
+        notas.innerText = "";
+        for (const materia in materias) {
+            const tr = document.createElement("tr");
+            const thMateria = document.createElement("th");
+            thMateria.innerText = materia;
+            tr.appendChild(thMateria);
+            // for (const nota of alunos.notas[materia]){
+            //     const tdNota = document.createElement("td");
+            //     tdNota!.innerText = String(nota);
+            //     console.log(tdNota);
+            //     tr.appendChild(tdNota);
+            // for (const faltas of alunos.faltas[materia]){
+            //     const tdFalta = document.createElement("td");
+            //     tdFalta!.innerText = String(faltas);
+            //     console.log(tdFalta);
+            //     tr.appendChild(tdFalta);
+            // }
+            // }
+            notas === null || notas === void 0 ? void 0 : notas.appendChild(tr);
+        }
+    });
 }
 export {};
--- src/details.ts
+++ src/details.ts
@@ -1,41 +1,79 @@
 import {ITurma , IAlunos} from "./type";
 
+let alunos: IAlunos
+let turmas: ITurma[] = []
+let materias:any = {}
+
+
+// Puxar turmas
+async function getTurma (){
+    const resp = await fetch ("http://127.0.0.1:3500/lista-turmas");
+    const turma = await resp.json();
+    // console.log(turma);
+    turmas = turma;
+}
+
+// Puxar materias
+async function getMateria(){
+    const resp = await fetch ("http://127.0.0.1:3500/lista-materia");
+    const materia = await resp.json();
+    console.log(materia);
+    materias = materia;
+}
+
+// Puxar alunos
+async function getStudent (id:number){
+    const resp =  await fetch (`http://localhost:3500/lista-alunos/${id}`);
+    const aluno = await resp.json();    
+    // console.log(aluno);
+    alunos = aluno;     
+} 
+
 
 const values = window.location.search;
 const searchParams = new URLSearchParams(values);
 
-
-
-async function getStudent (id:number){
-    const resp =  await fetch (`http://localhost:3500/lista-alunos/${id}`);
-    const data = await resp.json();    
-    console.log(data);   
-    
-    function dataStudent (id2:number) {    
-    const cont = document.querySelector(".container");
-    if (id2 === id){            
-    
-    cont!.innerHTML = 
-        `<span>Nome: ${data.nome}</span><br />
-        <span>Turma: ${data.turma}</span><br />`  
-        } 
-        
-        else {
-           cont!.innerHTML = `Página Invalida` 
-        }
-
-    }
-
-    dataStudent(data.id) 
-}
-    
   if (searchParams.has("id")){
     console.log("id", searchParams.get("id"));
     const id = parseInt(searchParams.get("id")!);
-    getStudent(id);
-} 
 
+    Promise.all ([getMateria(), getTurma(), getStudent(id)]).then(() =>{
+        console.log(alunos, turmas, materias);
+        
+        // popular nome aluno
+        document.querySelector("#nome")!.textContent = alunos.nome;
+        
+        // popular turma do aluno
+        const turma = turmas.find ((turma)=> turma.id === alunos.turma);
+        document.querySelector("#turma")!.textContent = turma?.turma;
 
+        // popular materias
+        const notas = document.querySelector("tbody");
+        console.log(notas);
+        notas!.innerText="";
 
-   
-
+        for (const materia in materias){
+            
+            const tr = document.createElement("tr");
+            const thMateria = document.createElement("th");
+            thMateria.innerText = materia;
+            tr.appendChild(thMateria);
+            
+            
+            // for (const nota of alunos.notas[materia]){
+            //     const tdNota = document.createElement("td");
+            //     tdNota!.innerText = String(nota);
+            //     console.log(tdNota);
+            //     tr.appendChild(tdNota);
+            
+                // for (const faltas of alunos.faltas[materia]){
+                //     const tdFalta = document.createElement("td");
+                //     tdFalta!.innerText = String(faltas);
+                //     console.log(tdFalta);
+                //     tr.appendChild(tdFalta);
+                // }
+            // }
+            notas?.appendChild(tr)
+        }    
+    })
+  }
